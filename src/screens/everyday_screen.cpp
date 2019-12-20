f4a50d587e9efb28c138d6e5651d644a3a60e730
timostrating <timostrating@home.nl>
2019-12-20 16:17:20 +0100

    yesterdays everyday

--- src/screens/everyday_screen.cpp
+++ src/screens/everyday_screen.cpp
@@ -17,6 +17,7 @@
 #include "../graphics/cubemap.h"
 
 using namespace glm;
+using namespace MATH;
 
 #define MULTILINE(...) #__VA_ARGS__
 
@@ -26,13 +27,12 @@ class EverydayScreen : public Screen
     constexpr static char vertSource[] = R"glsl(#version 300 es
         layout (location = 0) in vec3 a_pos;
 
-        out vec3 TexCoords;
-
         uniform mat4 MVP;
 
-        void main()
-        {
-            TexCoords = a_pos;
+        out vec2 v_uv;
+
+        void main() {
+            v_uv = 1.0 - (a_pos.xy + 1.0) / 2.0;
             gl_Position = MVP * vec4(a_pos, 1.0);
         })glsl";
 
@@ -43,43 +43,40 @@ class EverydayScreen : public Screen
 
         out vec4 outputColor;
 
+        in vec2 v_uv;
         uniform float u_time;
+        uniform float u_alpha;
+        uniform sampler2D u_texture;
 
         void main() {
-            vec3 color = vec3(13.0/255.0);
-            float a = mod(gl_FragCoord.x + 30.0, 120.0);
-            float b = mod(gl_FragCoord.y + 30.0, 120.0);
-            float width = 1.0 + 19.0 * abs(cos(u_time * 0.5));
+            vec4 color = vec4(97.0/255.0, 116.0/255.0, 140.0/255.0, 1.0);
+            float noise = texture(u_texture, 0.2 * vec2(v_uv.x + 0.01 * u_time, v_uv.y + 0.015 * u_time)).x;
+            color += 0.7 * vec4(smoothstep(0.7, 0.9, noise));
 
-            if (a < width || b < width) color = vec3(64.0/255.0);
-            a -= width/2.0;
-            b -= width/2.0;
-            if (sqrt(a*a + b*b) < sqrt(2.0 * pow(width/2.0, 2.0))) color = vec3(1.0);
+            color += vec4(sqrt(pow(v_uv.x - 0.5, 2.0) + pow(v_uv.y - 0.5, 2.0)));
 
-            outputColor = vec4(color, 1.0);
+            outputColor = vec4(color.xyz, u_alpha);
         })glsl";
 
     FlyingCamera camera = FlyingCamera();
 
-    ShaderProgram shader = ShaderProgram(vertSource, fragSource);
-//    ShaderProgram shader = ShaderProgram::fromAssetFiles("shaders/lib/flat_color.vert", "shaders/lib/flat_color.frag");
+    ShaderProgram waterShader = ShaderProgram(vertSource, fragSource);
+    ShaderProgram shader = ShaderProgram::fromAssetFiles("shaders/lib/flat_color.vert", "shaders/lib/flat_color.frag");
+    Texture texture = Texture::fromAssetFile("textures/turbulance.jpg");
 
     Gizmos gizmos;
     SharedMesh cube = SharedMesh(Mesh::cube());
     SharedMesh quad = SharedMesh(Mesh::quad());
 
-
-    float randoms[999];
-
 public:
     EverydayScreen()
     {
         shader.use();
+        waterShader.use();
+        texture.bind(0, waterShader, "u_texture");
 
         VertexBuffer::uploadSingleMesh(cube);
         VertexBuffer::uploadSingleMesh(quad);
-
-        for (float & i : randoms) i = MATH::random();
     }
 
     void setup(GLFWwindow* window) {}
@@ -89,10 +86,11 @@ public:
 
     void render(double deltaTime) {
         time += deltaTime;
-        glUniform1f(shader.uniformLocation("u_time"), time);
+        waterShader.use();
+        glUniform1f(waterShader.uniformLocation("u_time"), time);
 
-        //        float t2 = 8.0f * sin(time * 0.05);
-        glClearColor(1.0, 1.0, 1.0, 1.0);
+//        glClearColor(97.0/255.0, 116.0/255.0, 140.0/255.0, 1.0);
+        glClearColor(0.8 * 208.0/255.0, 0.8 * 217.0/255.0, 0.8 * 242.0/255.0, 1.0);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         if (anyKeyPressed) {
@@ -101,16 +99,43 @@ public:
         } else {
             if (INPUT::KEYBOARD::pressed(GLFW_KEY_TAB))
                 anyKeyPressed = true;
-            camera.position = vec3(0.001, 0.001, 2);
+            camera.position = vec3(1.5, 1, 1.5);
             camera.lookAt(vec3(0, 0, 0));
             camera.Camera::update();
         }
 
-        shader.use();
         /////////////////////////////////////////////////////////////////////////////////////////////////////////// TEST
 
-        glUniformMatrix4fv(shader.uniformLocation("MVP"), 1, GL_FALSE, &(camera.combined * scale(translate(mat4(1.0f), vec3(0, 0, 0)), 0.1f * VEC3::ONE))[0][0]);
+//        glUniformMatrix4fv(waterShader.uniformLocation("MVP"), 1, GL_FALSE, &(camera.combined * rotate(translate(mat4(1.0f), vec3(0, -0.05, 0)), radians(-90.0f), VEC3::X))[0][0]);
+//        quad->render();
+
+        shader.use();
+        srand(0);
+        for (float v=0.0; v< 2*PI; v += 0.01f) {
+            float gray = random(0.6f, 0.9f);
+            glUniform4f(shader.uniformLocation("u_color"), gray * 43.0/255.0, gray * 54.0/255.0, gray * 64.0/255.0, 1.0);
+            glUniformMatrix4fv(shader.uniformLocation("MVP"), 1, GL_FALSE, &(camera.combined * rotate(scale(translate(mat4(1.0f), random(1.0f, 1.5f) * vec3(sin(v), 0, cos(v))), random(0.05f, 0.1f) * VEC3::ONE), radians(random(0.0f, 360.0f)), VEC3::X))[0][0]);
+            cube->render();
+        }
+
+        for (float v=0.0; v< 2*PI; v += 0.1f) {
+//            glUniform4f(shader.uniformLocation("u_color"), 93.0/255.0, 145.0/255.0, 125.0/255.0, 1.0);
+            glUniform4f(shader.uniformLocation("u_color"), 43.0/255.0, 64.0/255.0, 54.0/255.0, 1.0);
+            glUniformMatrix4fv(shader.uniformLocation("MVP"), 1, GL_FALSE, &(camera.combined * rotate(scale(translate(mat4(1.0f), random(1.0f, 1.5f) * vec3(sin(v), 0.08, cos(v))), random(0.02f, 0.05f) * VEC3::ONE), radians(random(0.0f, 360.0f)), VEC3::X))[0][0]);
+            cube->render();
+        }
+
+        waterShader.use();
+        glUniform1f(waterShader.uniformLocation("u_time"), time);
+
+        glUniformMatrix4fv(waterShader.uniformLocation("MVP"), 1, GL_FALSE, &(camera.combined * rotate(translate(mat4(1.0f), vec3(0, -0.02, 0)), radians(-90.0f), VEC3::X))[0][0]);
+        glUniform1f(waterShader.uniformLocation("u_alpha"), 1.0);
         quad->render();
+        glUniformMatrix4fv(waterShader.uniformLocation("MVP"), 1, GL_FALSE, &(camera.combined * rotate(translate(mat4(1.0f), vec3(0, 0.02, 0)), radians(-90.0f), VEC3::X))[0][0]);
+        glUniform1f(waterShader.uniformLocation("u_alpha"), 0.1);
+        quad->render();
+
+
     }
 
     void resize(int width, int height)
