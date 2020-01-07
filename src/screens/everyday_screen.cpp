ce0c389a7cb0720ac02cc9863754399aa5807d20
timostrating <timostrating@home.nl>
2020-01-07 23:10:31 +0100

    everyday toon lighting

--- src/screens/everyday_screen.cpp
+++ src/screens/everyday_screen.cpp
@@ -6,6 +6,9 @@
 #include <glm/glm.hpp>
 #include "glm/gtx/rotate_vector.hpp"
 #include <GLFW/glfw3.h>
+#include <imgui.h>
+#include <examples/imgui_impl_glfw.h>
+#include <examples/imgui_impl_opengl3.h>
 
 #include "../util/interfaces/screen.h"
 #include "../graphics/shader_program.h"
@@ -27,12 +30,10 @@ class EverydayScreen : public Screen
         uniform mat4 MVP;
         uniform vec4 u_color;
 
-        out vec3 v_color;
         out vec3 v_pos;
 
         void main() {
             v_pos = a_pos;
-            v_color = vec3(u_color.r + (a_pos.x), u_color.g + (a_pos.y), u_color.b + (a_pos.z));
             gl_Position = MVP * vec4(a_pos, 1.0);
         })glsl";
 
@@ -45,19 +46,26 @@ class EverydayScreen : public Screen
 
         uniform float u_time;
 
-        in vec3 v_color;
         in vec3 v_pos;
 
         void main() {
-//            if (sqrt(pow(v_pos.x, 2.0) + pow(v_pos.y, 2.0) + pow(v_pos.z, 2.0)) < (abs(sin(u_time*0.5)) + 0.5) )
-//                discard;
-            outputColor = vec4(v_color, 1.0);
+            vec3 light_pos = vec3(sin(u_time) * 3.0, 3.0, cos(u_time) * 3.0);
+            float NdotL1 = dot(normalize(v_pos), normalize(light_pos));
+
+            if(NdotL1 > 0.5) {
+                outputColor        =  vec4(50.0/255.0, 165.0/255.0, 65.0/255.0, 1.0);
+            } else if(NdotL1 > 0.2) {
+                outputColor        =  vec4(28.0/255.0, 135.0/255.0, 80.0/255.0, 1.0);
+            } else {
+                outputColor        =  vec4(24.0/255.0, 62.0/255.0, 46.0/255.0, 1.0);
+            }
+//            outputColor = vec4(v_color, 1.0);
         })glsl";
 
     FlyingCamera camera = FlyingCamera();
 
     ShaderProgram shader = ShaderProgram(vertSource, fragSource);
-//    ShaderProgram shader = ShaderProgram::fromAssetFiles("shaders/lib/flat_color.vert", "shaders/lib/flat_color.frag");
+    ShaderProgram flatShader = ShaderProgram::fromAssetFiles("shaders/lib/flat_color.vert", "shaders/lib/flat_color.frag");
 //    Texture texture = Texture::fromAssetFile("textures/tur.jpg");
 
     Gizmos gizmos;
@@ -80,18 +88,31 @@ public:
 //        texture.bind(0, shader, "u_texture");
     }
 
-    void setup(GLFWwindow* window) {}
 
+    void setup(GLFWwindow* window)
+    {
+        ImGui::CreateContext();
+        ImGuiIO& io = ImGui::GetIO();
+        // TODO: Set optional io.ConfigFlags values, e.g. 'io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard' to enable keyboard controls.
+        // TODO: Fill optional fields of the io structure later.
+        // TODO: Load TTF/OTF fonts if you don't want to use the default font.
+
+        const char* glsl_version = "#version 300 es"; // TODO: We should not hardcoded that here
+
+        // Initialize helper Platform and Renderer bindings (here we are using imgui_impl_win32 and imgui_impl_dx11)
+        ImGui_ImplGlfw_InitForOpenGL(window, true);
+        ImGui_ImplOpenGL3_Init(glsl_version);
+    }
 
     double time = 0;
     bool anyKeyPressed = false;
-    float edit1 = 1.7;
 
     void render(double deltaTime) {
         time += deltaTime;
+        shader.use();
         glUniform1f(shader.uniformLocation("u_time"), time);
 
-        glClearColor(154.0/255.0, 147.0/255.0, 135.0/255.0, 1.0);
+        glClearColor(0.3 * 171.0/255.0, 0.3 * 150.0/255.0, 0.3 * 83.0/255.0, 1.0);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         if (anyKeyPressed) {
@@ -100,67 +121,41 @@ public:
         } else {
             if (INPUT::KEYBOARD::pressed(GLFW_KEY_TAB))
                 anyKeyPressed = true;
-            camera.position = vec3(sin(time * 0.5) * 3, 1, cos(time * 0.5) * 3);
-            camera.lookAt(vec3(0, 0, 0));
+            camera.position = vec3(7, 3, 7);
+            camera.lookAt(vec3(0, 2, 0));
             camera.Camera::update();
         }
 
         /////////////////////////////////////////////////////////////////////////////////////////////////////////// TEST
+
         shader.use();
-        if (INPUT::KEYBOARD::pressed(GLFW_KEY_Q))
-            edit1 -= 0.01;
-        if (INPUT::KEYBOARD::pressed(GLFW_KEY_W))
-            edit1 += 0.01;
-        std::cout << edit1 << "\n";
+        glUniformMatrix4fv(shader.uniformLocation("MVP"), 1, GL_FALSE, &(camera.combined * translate(mat4(1), vec3(0,0,0)))[0][0]);
+        sphere->render();
 
-//        line.debugDraw(&gizmos);
+        flatShader.use();
+        glUniformMatrix4fv(flatShader.uniformLocation("MVP"), 1, GL_FALSE, &(camera.combined * scale(translate(mat4(1), vec3(sin(time)*3,3,cos(time)*3)), 0.5f * vec3(1)))[0][0]);
+        glUniform4f(flatShader.uniformLocation("u_color"), 1.0, 1.0, 0.8, 1.0);
+        sphere->render();
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////// GUI
+//        shader.use();
+//        VertexBuffer::bindDefault();
 //
-//        for (int i=0; i<line.length; i++) {
-//            vec3 position = line.getPointPosition(i);
+//        // Feed inputs to dear imgui, start new frame
+//        ImGui_ImplOpenGL3_NewFrame();
+//        ImGui_ImplGlfw_NewFrame();
+//        ImGui::NewFrame();
 //
-//            for (float t=0; t<fmodf(time * 0.1, 1.0); t+=0.1)
-//                for (float f=0; f<2*PI; f += 0.1)
-//                    gizmos.drawLine(vec3(sin(f),cos(f), 0), vec3(sin(f+0.5),cos(f+0.5), 0), COLOR::PINK, line.lerpIndexed(i, t));
-//        }
-        vec4 color = vec4(0, 0, 0, 1.0);
-
-        int MAX = 50;
-        float phiStep = PI / MAX;
-        float thetaStep = 2*PI / MAX;
-
-        float t = remap(sin(time*0.5), -1, 1, 0.2, 1.5);
-        float t2 = remap(cos(time*0.5), -1, 1, 0.2, 1.5);
-
-        for (int y=0; y<MAX; y ++) // [-90,90]
-        {
-            float phi = remap(y, 0, MAX, -0.5*PI, 0.5*PI);
-            float r1  = supershape(phi, 5, t, t2, t2);
-            float r1s = supershape(phi+phiStep, 5, t, t2, t2);
-            for (int x=0; x<MAX; x++) // [0,360]
-            {
-                float theta = remap(x, 0, MAX, 0, 2*PI);
-                float r2  = supershape(theta, 5, t, t2, t2);
-                float r2s = supershape(theta+thetaStep, 5, t, t2, t2);
-
-                vec3 a1 = vec3(r1  * cos(phi)         * r2  * cos(theta),             r1  * sin(phi),           r1  * cos(phi)         * r2  * sin(theta));
-                vec3 a2 = vec3(r1  * cos(phi)         * r2s * cos(theta+thetaStep),   r1  * sin(phi),           r1  * cos(phi)         * r2s * sin(theta+thetaStep));
-                vec3 b1 = vec3(r1s * cos(phi+phiStep) * r2  * cos(theta),             r1s * sin(phi+phiStep),   r1s * cos(phi+phiStep) * r2  * sin(theta));
-                vec3 b2 = vec3(r1s * cos(phi+phiStep) * r2s * cos(theta+thetaStep),   r1s * sin(phi+phiStep),   r1s * cos(phi+phiStep) * r2s * sin(theta+thetaStep));
-                gizmos.drawLine(a1, a2, color);
-                gizmos.drawLine(a1, b1, color);
-                gizmos.drawLine(a1, b2, color);
-            }
-        }
-
-//        std::cout << " " << y << " " << x/y << "\n";
-    }
-
-    const float a = 1.0;
-    const float b = 1.0;
-
-    float supershape(float theta, float m, float n1, float n2, float n3)
-    {
-        return pow(   pow(abs((1.0/a) * cos(m * theta / 4.0)), n2)  +  pow(abs((1.0/b) * sin(m * theta / 4.0)), n3),   -1.0/n1);
+//        ImGui::Begin("LSystem pattern");
+//        ImGui::SliderFloat("m", &m, -10, 10);
+//        ImGui::SliderFloat("n1", &n1, -10, 10);
+//        ImGui::SliderFloat("n2", &n2, -10, 10);
+//        ImGui::SliderFloat("n3", &n3, -10, 10);
+//        ImGui::End();
+//
+//        // Render dear imgui into screen
+//        ImGui::Render();
+//        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
     }
 
     void resize(int width, int height)
