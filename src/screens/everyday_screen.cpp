52dd5a6d78f1f298cd085722670f643962fda7c2
timostrating <timostrating@home.nl>
2020-02-21 23:24:57 +0100

    hsv en hsl color space everydays

--- src/screens/everyday_screen.cpp
+++ src/screens/everyday_screen.cpp
@@ -77,157 +77,6 @@ class EverydayScreen : public Screen
             return samplePosition / scale;
         }
 
-
-        float scene(vec2 position) {
-            vec2 pos = position;
-            pos = translate(pos, vec2(0.0, -1.0));
-            float pulseScale = 1.7 + 0.2*sin(u_time * PI);
-            pos = scale(pos, pulseScale);
-            pos = rotate(pos, (0.52 + 0.01 *sin(u_time)) * PI);
-            float sceneDistance = rectangle(pos, vec2(1.0)) * pulseScale;
-            sceneDistance = min(sceneDistance, circle(translate(pos, vec2(1.2, 0.0)), 1.0) * pulseScale);
-            sceneDistance = min(sceneDistance, circle(translate(pos, vec2(0.0, -1.2)), 1.0) * pulseScale);
-
-            return sceneDistance;
-        }
-
-//        void main() {
-//            float dist = scene(v_pos.xy * 5.0);
-//            vec3 color1 = vec3(213.0/255.0,   3.0/255.0,  25.0/255.0);
-//            vec3 color2 = vec3(255.0/255.0, 195.0/255.0, 203.0/255.0);
-//
-//            outputColor = vec4(vec3(mix(mix(vec3(0.0), color2, smoothstep(0.5, 0.51, dist)), color1, smoothstep(0.5, 0.3, dist))), 1.0);
-//        }
-
-        // http://glslsandbox.com/e#61256.0
-        vec2 uv;
-
-        const vec2 ch_size  = vec2(1.0, 2.0) * 0.6;              // character size (X,Y)
-        const vec2 ch_space = ch_size + vec2(1.0, 1.0);    // character distance Vector(X,Y)
-        const vec2 ch_start = vec2 (ch_space.x, 1.); // start position
-              vec2 ch_pos   = vec2 (0.0, 0.0);             // character position(X,Y)
-        //      vec3 ch_color = vec3 (1.5, 0.75, 0.5);        // character color (R,G,B)
-        //const vec3 bg_color = vec3 (0.2, 0.2, 0.2);        // background color (R,G,B)
-
-        #define REPEAT_SIGN false // True/False; True=Multiple, False=Single
-
-        /* 16 segment display...Akin to LED Display.
-
-        Segment bit positions:
-          __2__ __1__
-         |\    |    /|
-         | \   |   / |
-         3  11 40 9  0
-         |   \ | /   |
-         |    \|/    |
-          _12__ __8__
-         |           |
-         |    /|\    |
-         4   / | \   7
-         | 13 14  15 |
-         | /   |   \ |
-          __5__|__6__
-
-        15 12 11 8 7  4 3  0
-         |  | |  | |  | |  |
-         0000 0000 0000 0000
-
-        example: letter A
-
-           12    8 7  4 3210
-            |    | |  | ||||
-         0001 0001 1001 1111
-
-         binary to hex -> 0x119F
-        */
-
-        #define n0 ddigit(0x22FF);
-        #define n1 ddigit(0x0281);
-        #define n2 ddigit(0x1177);
-        #define n3 ddigit(0x11E7);
-        #define n4 ddigit(0x5508);
-        #define n5 ddigit(0x11EE);
-        #define n6 ddigit(0x11FE);
-        #define n7 ddigit(0x2206);
-        #define n8 ddigit(0x11FF);
-        #define n9 ddigit(0x11EF);
-
-        #define A ddigit(0x119F);
-        #define B ddigit(0x927E);
-        #define C ddigit(0x007E);
-        #define D ddigit(0x44E7);
-        #define E ddigit(0x107E);
-        #define F ddigit(0x101E);
-        #define G ddigit(0x807E);
-        #define H ddigit(0x1199);
-        #define I ddigit(0x4466);
-        #define J ddigit(0x4436);
-        #define K ddigit(0x9218);
-        #define L ddigit(0x0078);
-        #define M ddigit(0x0A99);
-        #define N ddigit(0x8899);
-        #define O ddigit(0x00FF);
-        #define P ddigit(0x111F);
-        #define Q ddigit(0x80FF);
-        #define R ddigit(0x911F);
-        #define S ddigit(0x8866);
-        #define T ddigit(0x4406);
-        #define U ddigit(0x00F9);
-        #define V ddigit(0x2218);
-        #define W ddigit(0xA099);
-        #define X ddigit(0xAA00);
-        #define Y ddigit(0x4A00);
-        #define Z ddigit(0x2266);
-        #define _ ch_pos.x += ch_space.x;
-        #define s_dot     ddigit(0);
-        #define s_minus   ddigit(0x1100);
-        #define s_plus    ddigit(0x5500);
-        #define s_greater ddigit(0x2800);
-        #define s_less    ddigit(0x8200);
-        #define s_sqrt    ddigit(0x0C02);
-        #define nl1 ch_pos = ch_start;  ch_pos.y -= 3.0;
-        #define nl2 ch_pos = ch_start;  ch_pos.y -= 6.0;
-        #define nl3 ch_pos = ch_start;	ch_pos.y -= 9.0;
-
-        float dseg(vec2 p0, vec2 p1)
-        {
-            vec2 dir = normalize(p1 - p0);
-            vec2 cp = (uv - ch_pos - p0) * mat2(dir.x, dir.y,-dir.y, dir.x);
-            return distance(cp, clamp(cp, vec2(0), vec2(distance(p0, p1), 0)));
-        }
-
-        bool bit(int n, int b)
-        {
-            return mod(floor(float(n) / exp2(floor(float(b)))), 2.0) != 0.0;
-        }
-
-        float d = 1e6;
-
-        void ddigit(int n)
-        {
-            float v = 1e6;
-            vec2 cp = uv - ch_pos;
-            if (n == 0)     v = min(v, dseg(vec2(-0.405, -1.000), vec2(-0.500, -1.000)));
-            if (bit(n,  0)) v = min(v, dseg(vec2( 0.500,  0.063), vec2( 0.500,  0.937)));
-            if (bit(n,  1)) v = min(v, dseg(vec2( 0.438,  1.000), vec2( 0.063,  1.000)));
-            if (bit(n,  2)) v = min(v, dseg(vec2(-0.063,  1.000), vec2(-0.438,  1.000)));
-            if (bit(n,  3)) v = min(v, dseg(vec2(-0.500,  0.937), vec2(-0.500,  0.062)));
-            if (bit(n,  4)) v = min(v, dseg(vec2(-0.500, -0.063), vec2(-0.500, -0.938)));
-            if (bit(n,  5)) v = min(v, dseg(vec2(-0.438, -1.000), vec2(-0.063, -1.000)));
-            if (bit(n,  6)) v = min(v, dseg(vec2( 0.063, -1.000), vec2( 0.438, -1.000)));
-            if (bit(n,  7)) v = min(v, dseg(vec2( 0.500, -0.938), vec2( 0.500, -0.063)));
-            if (bit(n,  8)) v = min(v, dseg(vec2( 0.063,  0.000), vec2( 0.438, -0.000)));
-            if (bit(n,  9)) v = min(v, dseg(vec2( 0.063,  0.063), vec2( 0.438,  0.938)));
-            if (bit(n, 10)) v = min(v, dseg(vec2( 0.000,  0.063), vec2( 0.000,  0.937)));
-            if (bit(n, 11)) v = min(v, dseg(vec2(-0.063,  0.063), vec2(-0.438,  0.938)));
-            if (bit(n, 12)) v = min(v, dseg(vec2(-0.438,  0.000), vec2(-0.063, -0.000)));
-            if (bit(n, 13)) v = min(v, dseg(vec2(-0.063, -0.063), vec2(-0.438, -0.938)));
-            if (bit(n, 14)) v = min(v, dseg(vec2( 0.000, -0.938), vec2( 0.000, -0.063)));
-            if (bit(n, 15)) v = min(v, dseg(vec2( 0.063, -0.063), vec2( 0.438, -0.938)));
-            ch_pos.x += ch_space.x;
-            d = min(d, v);
-        }
-
         vec3 hsv2rgb_smooth( in vec3 c )
         {
             vec3 rgb = clamp( abs(mod(c.x*6.0+vec3(0.0,4.0,2.0),6.0)-3.0)-1.0, 0.0, 1.0 );
@@ -236,24 +85,33 @@ class EverydayScreen : public Screen
 
             return c.z * mix( vec3(1.0), rgb, c.y);
         }
+
+        float f( float H, float Sl, float L, float n )
+        {
+            float k = mod((n + H/30.0), 30.0);
+            float a = Sl * min(L, 1.0-L);
+            return L - a * max(-1.0, min(min(k-3.0, 9.0-k), 1.0));
+        }
+
+        // https://en.wikipedia.org/wiki/HSL_and_HSV#HSL_to_RGB_alternative
+        vec3 hsl2rgb( in vec3 c )
+        {
+            float H = mod(c.x, 1.0) * 360.0;
+            float Sl = mod(c.y, 1.0);
+            float L = mod(c.z, 1.0);
+
+            float R = f(H,Sl,L, 0.0);
+            float G = f(H,Sl,L, 8.0);
+            float B = f(H,Sl,L, 4.0);
+
+            return vec3(R, G, B);
+        }
+
         void main( void )
         {
-            vec2 aspect = u_resolution.xy / u_resolution.y;
-            uv = ( gl_FragCoord.xy / u_resolution.y ) - aspect / 2.0;
-            uv *= 15.0 + 3.0*abs(cos(u_time * PI));
-            uv = translate(uv, vec2(-7., -1.));
-            ch_pos = ch_start;
+            vec2 pos = gl_FragCoord.xy/vec2(800.0);
 
-            S D F _ T E X T
-
-            vec3 color2 = vec3(221.0/255.0, 255.0/255.0, 255.0/255.0);
-            vec3 color1 = 0.95*color2; //vec3(255.0/255.0, 235.0/255.0, 183.0/255.0);
-
-            vec2 pos = ( gl_FragCoord.xy / u_resolution.y ) - aspect / 2.0;
-            float oke = clamp(circle(pos, 0.3), 0.0, 1.0);
-
-            vec3 color = vec3(-1.0 * oke) + mix(color1, color2, 1.0- ((0.02 + 0.9*abs(sin(u_time * PI))) / d));  // shading
-            outputColor = vec4(color, 1.0);
+            outputColor = vec4(hsl2rgb(vec3(pos + vec2(u_time, 0), (sin(u_time) + 1.0)*0.5)), 1.0);
         }
 
 )glsl";
